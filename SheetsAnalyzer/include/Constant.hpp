3dd35d9cc7fa96dfa57378821634fa793011c80b
Ryoga-exe <git@ryoga.dev>
2025-07-18 05:29:14 +0900

    feat: add Metadata

---
+++ SheetsAnalyzer/include/Constant.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+#define NO_S3D_USING
+#include <Siv3D.hpp>
+
+namespace SheetsAnalyzer {
+    namespace Constant {
+        constexpr s3d::StringView DefaultTitle = U"Untitled";
+        constexpr s3d::StringView DefaultTitleSort = U"Untitled";
+        constexpr s3d::StringView DefaultArtist = U"Unknown Artist";
+        constexpr s3d::StringView DefaultDesiner = U"Anonymous";
+        constexpr s3d::StringView DefaltGenre = U"Unspecified";
+        constexpr double DefaultMusicOffset = 0.0;
+        constexpr double DefaultBPM = 120.0;
+
+        constexpr size_t MaxDifficulties = 10;
+    }
+}
---
+++ SheetsAnalyzer/include/Metadata.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+#define NO_S3D_USING
+#include <Siv3D.hpp>
+#include "Constant.hpp"
+
+namespace SheetsAnalyzer {
+    struct Difficulty {
+        double level = 0.0;
+        s3d::FilePath src;
+        s3d::String desiner { Constant::DefaultDesiner };
+    };
+
+    struct Metadata {
+        s3d::String id;
+        s3d::String title { Constant::DefaultTitle };
+        s3d::String title_sort { Constant::DefaultTitleSort };
+        s3d::String artist { Constant::DefaultArtist };
+        s3d::String genre { Constant::DefaltGenre };
+        s3d::String music;
+        s3d::String jacket;
+        s3d::URL url;
+        double music_offset { Constant::DefaultMusicOffset };
+        double bpm { Constant::DefaultBPM };
+
+        std::array<Difficulty, Constant::MaxDifficulties> difficulties;
+    };
+}
