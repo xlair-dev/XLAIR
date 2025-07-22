108d7685b8e4ac38cfa0813db2231ec266a4d595
Ryoga-exe <git@ryoga.dev>
2025-07-23 02:27:11 +0900

    feat: add inline

--- SheetsAnalyzer/include/Constant.hpp
+++ SheetsAnalyzer/include/Constant.hpp
@@ -4,16 +4,16 @@
 
 namespace SheetsAnalyzer {
     namespace Constant {
-        constexpr s3d::int32 Version = 1;
+        constexpr inline s3d::int32 Version = 1;
 
-        const s3d::String DefaultTitle = U"Untitled";
-        const s3d::String DefaultTitleSort = U"Untitled";
-        const s3d::String DefaultArtist = U"Unknown Artist";
-        const s3d::String DefaltGenre = U"Unspecified";
-        const s3d::String DefaultDesigner = U"Anonymous";
-        constexpr double DefaultMusicOffset = 0.0;
-        constexpr double DefaultBPM = 120.0;
+        const inline s3d::String DefaultTitle = U"Untitled";
+        const inline s3d::String DefaultTitleSort = U"Untitled";
+        const inline s3d::String DefaultArtist = U"Unknown Artist";
+        const inline s3d::String DefaltGenre = U"Unspecified";
+        const inline s3d::String DefaultDesigner = U"Anonymous";
+        constexpr inline double DefaultMusicOffset = 0.0;
+        constexpr inline double DefaultBPM = 120.0;
 
-        constexpr size_t MaxDifficulties = 10;
+        constexpr inline size_t MaxDifficulties = 10;
     }
 }
