3198fd6306a0c2944b740f25315d68a52a862719
Ryoga-exe <git@ryoga.dev>
2025-09-16 22:53:28 +0900

    feat: add Difficulty

---
+++ XLAIR/src/core/types/Difficulty.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "Common.hpp"
+
+namespace core::types {
+    // TODO: Determine the difficulty name
+    enum class Difficulty : uint8 {
+        Basic,
+        Advanced,
+        Master,
+        Unknown,
+    };
+
+    inline Difficulty DifficultyFromInt(const uint8 value) {
+        switch (value) {
+            case 0: return Difficulty::Basic;
+            case 1: return Difficulty::Advanced;
+            case 2: return Difficulty::Master;
+            default: return Difficulty::Unknown;
+        }
+    }
+
+    inline String ToString(Difficulty value) {
+        switch (value) {
+            case Difficulty::Basic: return U"Basic";
+            case Difficulty::Advanced: return U"Advanced";
+            case Difficulty::Master: return U"Master";
+            case Difficulty::Unknown: return U"Unknown";
+        }
+    }
+}
