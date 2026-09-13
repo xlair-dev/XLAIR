7270a20600f840abc08afe37e884da1b8e39a62d
Ryoga-exe <git@ryoga.dev>
2026-09-13 22:29:16 +0900

    feat: impl ApiClient

---
+++ libs/ApiClient/include/ApiClient/HttpClient.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <ApiClient/ClientOptions.hpp>
+#include <ApiClient/IClient.hpp>
+
+namespace xlair::api {
+    struct HttpSession;
+
+    class HttpClient final : public IClient {
+    public:
+        explicit HttpClient(const ClientOptions& config);
+
+        Request<User> findUserByCard(s3d::StringView card_id) override;
+
+        Request<s3d::Array<UserRecord>> fetchUserRecords(s3d::StringView user_id) override;
+
+        Request<s3d::Array<UserRecord>>
+        submitPlayRecords(s3d::StringView user_id, const s3d::Array<RecordSubmission>& records) override;
+
+        Request<PlayOptions> fetchPlayOptions(s3d::StringView user_id) override;
+
+        Request<PlayOptions> savePlayOptions(s3d::StringView user_id, const PlayOptions& options) override;
+
+        Request<s3d::uint32> incrementCredits(s3d::StringView user_id) override;
+
+        Request<s3d::Array<Music>> fetchCatalog() override;
+
+    private:
+        std::shared_ptr<HttpSession> m_session;
+    };
+}
